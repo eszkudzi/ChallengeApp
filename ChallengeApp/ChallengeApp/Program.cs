ac7b4e253e9d4b0dd11182051af5bd3620db9953
eszkudzi <ewa.szkudzinska@gmail.com>
2023-08-19 10:42:45 +0200

    Homework #3

--- ChallengeApp/ChallengeApp/Program.cs
+++ ChallengeApp/ChallengeApp/Program.cs
@@ -1,1 +1,20 @@
-﻿Console.WriteLine("Hello, Ewa!");
+﻿string name = "Ewa";
+char gender = 'k'; //k - kobieta, m - mężczyzna
+int age = 30;
+
+if (gender == 'k' && age < 30)
+{
+    Console.WriteLine("Kobieta poniżej 30 lat");
+}
+else if (name == "Ewa" && age == 33)
+{
+    Console.WriteLine("Ewa, lat 33");
+}
+else if (gender != 'k' && age < 18)
+{
+    Console.WriteLine("Niepełnoletni Mężczyzna");
+}
+else
+{
+    Console.WriteLine("Inny człowiek");
+}
