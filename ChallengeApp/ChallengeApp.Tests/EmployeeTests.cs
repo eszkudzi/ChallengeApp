ddefc8a01a75cc1a331afeac6ca1c79b8dbf157b
eszkudzi <ewa.szkudzinska@gmail.com>
2023-08-21 17:32:50 +0200

    Homework day 7 - unit tests

---
+++ ChallengeApp/ChallengeApp.Tests/EmployeeTests.cs
@@ -0,0 +1,59 @@
+namespace ChallengeApp.Tests
+{
+    public class Tests
+    {
+
+        [Test]
+        public void EmpolyeeHasOnlyNegativePoints()
+        {
+            //arrange
+            var employee = new Employee("Marcin", "Kowal", 18);
+            employee.AddScore(-1);
+            employee.AddScore(-2);
+            employee.AddScore(-3);
+
+            //act
+            var result = employee.Result;
+
+            //assert
+            Assert.AreEqual(-6, result);
+
+        }
+
+        [Test]
+        public void EmpolyeeHasOnlyPositivePoints()
+        {
+            //arrange
+            var employee = new Employee("Dariusz", "Mazurek", 44);
+            employee.AddScore(1);
+            employee.AddScore(2);
+            employee.AddScore(3);
+
+            //act
+            var result = employee.Result;
+
+            //assert
+            Assert.AreEqual(6, result);
+
+        }
+
+        [Test]
+        public void EmpolyeeHasSumEqualZero()
+        {
+            //arrange
+            var employee = new Employee("Dariusz", "Mazurek", 44);
+            employee.AddScore(2);
+            employee.AddScore(3);
+            employee.AddScore(-2);
+            employee.AddScore(-3);
+
+            //act
+            var result = employee.Result;
+
+            //assert
+            Assert.AreEqual(0, result);
+
+        }
+
+    }
+}
